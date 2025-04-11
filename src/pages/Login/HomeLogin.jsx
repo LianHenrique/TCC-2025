06acfa3d570160ca33fefdb411349fca18023185
Lian <lianhenrique1902@gmail.com>
2025-04-11 15:27:33 -0300

    Lian Henrique: ipdate

--- src/pages/Login/HomeLogin.jsx
+++ src/pages/Login/HomeLogin.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import styles from "./HomeLogin.module.css"
 import { useNavigate } from "react-router-dom";
+import AlertaLoginInvalido from "../../Components/AlertaLogin/AlertaLoginInvalido";
+
 
 const HomeLogin = () => {
   const [email, setEmail] = useState("");
@@ -13,25 +15,27 @@ const HomeLogin = () => {
       navigate('/estoque');
     }
     else{
-      alert("Login invalido (testes no momento)")
-      alert("Login ADM senha ADM")
+      <AlertaLoginInvalido />
     }
   }
 
   return (
-    <div className={styles.body}>
-      <h1>Login</h1>
-      <div>
-        <input className={styles.input} type="text" placeholder="E-mail" onChange={(e) => {
-          setEmail(e.target.value)
-        }} />
-      </div>
-      <div>
-        <input className={styles.input} type="password" placeholder="Senha" onChange={(e) => {
-          setSenha(e.target.value)
-        }}/>
+    <div>
+      <AlertaLoginInvalido />
+      <div className={styles.body}>
+        <h1>Login</h1>
+        <div>
+          <input className={styles.input} type="text" placeholder="E-mail" onChange={(e) => {
+            setEmail(e.target.value)
+          }} />
         </div>
-      <button className={styles.button} onClick={aoClicar}>Entrar</button>
+        <div>
+          <input className={styles.input} type="password" placeholder="Senha" onChange={(e) => {
+            setSenha(e.target.value)
+          }}/>
+          </div>
+        <button className={styles.button} onClick={aoClicar}>Entrar</button>
+      </div>
     </div>
   )
 }
