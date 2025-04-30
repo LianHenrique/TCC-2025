13857e924261c473246429ddd6256a0cca1036a4
KLEITONSOBRAL <kleiton.c@aluno.senai.br>
2025-04-30 20:19:52 -0300

    cores e icones adicionados

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,55 +1,47 @@
-import styles from "../Components.module.css"
+import styles from "../Components.module.css";
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
-
 const NavBar = () => {
-
-  const navItems = ["Para quem", "Soluções", "Preços"];
+  const navItems = ["Para quem  |", " Soluções  |", " Preços"];
 
   return (
-    <nav 
-    className={styles.NavBar}>
-      <h1 className={styles.FullH1}>StoryBox</h1>
-      <div 
-      className={styles.MinBaseButtons}>
+    <nav className={styles.NavBar}>
+      <h1 translate="no" className={styles.FullH1}>StoryBox</h1>
+
+      <div className={styles.MinBaseButtons}>
         <h1 translate="no">StoryBox</h1>
         <div>
-          <Button 
-          className={styles.Button}>
-            Cadastro</Button>
-          <Button 
-          className={styles.Button}>
+          <Button translate="no" className={styles.Button}>
+            Cadastro
+          </Button>
+          <Button translate="no" className={styles.Button}>
             Login
           </Button>
         </div>
       </div>
-      <div 
-      className={styles.DivButton}>
+
+      <div className={styles.DivButton}>
         <ButtonGroup>
-          {
-            navItems.map((item, index) => (
-              <Button 
-              key={index} 
-              className={styles.ButtonGroup}>
-                {item}
-              </Button>
-            ))
-          }
+          {navItems.map((item, index) => (
+            <Button key={index} className={styles.ButtonGroup}>
+              {item}
+            </Button>
+          ))}
         </ButtonGroup>
       </div>
-      <div 
-      className={styles.FullBaseButtons}>
-        <Button 
-        className={styles.Button}>
-          Cadastro</Button>
-        <Button 
-        className={styles.Button}>
+
+      <div className={styles.FullBaseButtons}>
+        <Button translate="no" className={styles.Button}>
+          Cadastro
+        </Button>
+        <Button translate="no" className={styles.Button}>
           Login
         </Button>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
+
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,52 +1,56 @@
-import CardsCurso from "../../components/Cards/CardsCurso"
-import CardsDescPlanoGratuito from "../../components/Cards/CardsDesc"
-import NavBar from "../../components/NavBar/NavBar"
-import Styles from "../app.module.css"
+import React from 'react';
+import CardsCurso from "../../components/Cards/CardsCurso";
+import CardsDescPlanoGratuito from "../../components/Cards/CardsDesc";
+import NavBar from "../../components/NavBar/NavBar";
+import Styles from "../app.module.css";
 
 function App() {
 
   return (
     <div>
+      {/* NavBar Component */}
       <NavBar />
 
+      {/* CardsDescPlanoGratuito Component */}
       <CardsDescPlanoGratuito 
-      titulo="Gerencie o estoque do seu negócio de maneira ágil"
-      desc="Com o StoreBox, você pode gerenciar seus estoques
-      de maneira eficiente, contendo relatórios e outras 
-      funcionalidades que com toda certeza te auxiliarão!"
-      botaoTxt="Teste gratis"
-      comBotão={true}
-      txtComBotao="Experimente gratis por 30 dias"/>
+        titulo="Gerencie o estoque do seu negócio de maneira ágil"
+        desc="Com o StoreBox, você pode gerenciar seus estoques de maneira eficiente, contendo relatórios e outras funcionalidades que com toda certeza te auxiliarão!"
+        botaoTxt="Teste grátis"
+        comBotão={true}
+        txtComBotao="Experimente grátis por 30 dias"
+      />
 
+      {/* CardsCurso Component */}
       <CardsCurso 
-      cards={[
-        {
-          titulo: "Plano gratuito",
-          req1: "Menor espaço",
-          req2: "Sem todas funcionalidades",
-          req3: "Usuario unico",
-        },
-        {
-          titulo: "Plano gratuito",
-          req1: "Menor espaço",
-          req2: "Sem todas funcionalidades",
-          req3: "Usuario unico",
-        },
-        {
-          titulo: "Plano gratuito",
-          req1: "Menor espaço",
-          req2: "Sem todas funcionalidades",
-          req3: "Usuario unico",
-        },
-        {
-          titulo: "Plano gratuito",
-          req1: "Menor espaço",
-          req2: "Sem todas funcionalidades",
-          req3: "Usuario unico",
-        },
-      ]}/>
+        cards={[
+          {
+            titulo: "Plano gratuito",
+            req1: "Menor espaço",
+            req2: "Sem todas funcionalidades",
+            req3: "Usuário único",
+          },
+          {
+            titulo: "Plano 1",
+            req1: "Espaço maior",
+            req2: "Recursos adicionais",
+            req3: "Usuário múltiplo",
+          },
+          {
+            titulo: "Plano 2",
+            req1: "Espaço ilimitado",
+            req2: "Todas funcionalidades",
+            req3: "Usuários ilimitados",
+          },
+          {
+            titulo: "Plano 3",
+            req1: "Espaço personalizável",
+            req2: "Funcionalidades extras",
+            req3: "Usuários ilimitados",
+          },
+        ]}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
