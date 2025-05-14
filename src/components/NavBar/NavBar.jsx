501780c7c5bdb1ef573e0e08899a570f99975157
Lian Henrique <lianhenrique1902@gmail.com>
2025-05-14 19:03:39 -0300

    navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,44 +1,15 @@
+import { useNavigate } from "react-router";
 import styles from "../Components.module.css";
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 const NavBar = () => {
-  const navItems = ["Para quem  |", " Soluções  |", " Preços"];
+
+  const navigate = useNavigate()
 
   return (
     <nav className={styles.NavBar}>
-      <h1 translate="no" className={styles.FullH1}>StoryBox</h1>
-
-      <div className={styles.MinBaseButtons}>
-        <h1 translate="no">StoryBox</h1>
-        <div>
-          <Button translate="no" className={styles.Button}>
-            Cadastro
-          </Button>
-          <Button translate="no" className={styles.Button}>
-            Login
-          </Button>
-        </div>
-      </div>
-
-      <div className={styles.DivButton}>
-        <ButtonGroup>
-          {navItems.map((item, index) => (
-            <Button key={index} className={styles.ButtonGroup}>
-              {item}
-            </Button>
-          ))}
-        </ButtonGroup>
-      </div>
-
-      <div className={styles.FullBaseButtons}>
-        <Button translate="no" className={styles.Button}>
-          Cadastro
-        </Button>
-        <Button translate="no" className={styles.Button}>
-          Login
-        </Button>
-      </div>
+      <h1 translate="no" onClick={() => {
+        navigate("/home")
+      }} className={styles.FullH1}>StoryBox</h1>
     </nav>
   );
 };
---
+++ src/pages/Estoque/Estoque.jsx
@@ -0,0 +1,11 @@
+import NavBar from "../../components/NavBar/NavBar"
+
+const Estoque = () => {
+  return (
+    <div>
+        <NavBar />
+    </div>
+  )
+}
+
+export default Estoque
