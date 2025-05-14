7e7c3cc4b816353b56b4129730621c42c2bd3dfb
Lian Henrique <lianhenrique1902@gmail.com>
2025-05-14 20:09:42 -0300

    Lian

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,10 @@
-import { useNavigate } from "react-router";
-import styles from "../Components.module.css";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 
 const NavBar = () => {
 
   return (
     <Navbar style={{ minHeight: "90px" }} bg="primary" data-bs-theme="dark">
-      <Navbar.Brand href="#" className="fs-1 p-3">StoryBox</Navbar.Brand>
+      <Navbar.Brand href="#" className="fs-2 p-3">StoryBox</Navbar.Brand>
       <Nav className="me-auto">
         <Nav.Link href="#relatorio">Relatorio</Nav.Link>
         <Nav.Link href="/estoque">Estoque</Nav.Link>
