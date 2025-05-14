87b31c450c5db35bce4aa98907b4361ce85cdfe5
Lian Henrique <lianhenrique1902@gmail.com>
2025-05-14 19:17:45 -0300

    navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,21 @@
 import { useNavigate } from "react-router";
 import styles from "../Components.module.css";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const NavBar = () => {
 
   const navigate = useNavigate()
 
   return (
-    <nav className={styles.NavBar}>
-      <h1 translate="no" onClick={() => {
-        navigate("/home")
-      }} className={styles.FullH1}>StoryBox</h1>
-    </nav>
+    <Navbar bg="primary" data-bs-theme="dark">
+          <h1>StoryBox</h1>
+          <Nav className="me-auto">
+            <Nav.Link href="#relatorio">Relatorio</Nav.Link>
+            <Nav.Link href="/estoque">Estoque</Nav.Link>
+            <Nav.Link href="#funcionario">Funcionarios</Nav.Link>
+            <Nav.Link href="#cardapio">Cardapio</Nav.Link>
+          </Nav>
+      </Navbar>
   );
 };
 
--- src/pages/Estoque/Estoque.jsx
+++ src/pages/Estoque/Estoque.jsx
@@ -4,6 +4,7 @@ const Estoque = () => {
   return (
     <div>
         <NavBar />
+        
     </div>
   )
 }
