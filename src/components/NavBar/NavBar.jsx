cb6d04587f35a4f11f70391fe7c339bf5dadb135
WESLEY PEDREIRA PASSOS ROCHA <w.rocha@labcetec.net>
2025-05-16 20:43:09 -0300

    mexendo na responsividade da tela de ralatórios

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,60 @@
-import { Nav, Navbar } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+
+import { BsShop } from "react-icons/bs";
+
+import { useContext } from "react"
+
+import { AuthContext } from "../../Contexts/UserContext"
 
 const NavBar = () => {
 
+  const usuarioNome = "Visitante"
+
   return (
-    <Navbar style={{ minHeight: "90px" }} bg="primary" data-bs-theme="dark">
-      <Navbar.Brand href="#" className="fs-1 p-3">StoryBox</Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link href="#relatorio">Relatorio</Nav.Link>
-        <Nav.Link href="/estoque">Estoque</Nav.Link>
-        <Nav.Link href="#funcionario">Funcionarios</Nav.Link>
-            <Nav.Link href="#cardapio">Cardapio</Nav.Link>
-      </Nav>
-    </Navbar>
+    <div>
+        <Navbar expand="md" bg="primary" data-bs-theme="dark">
+            <Container>
+                <Navbar.Brand href="/home">
+                    StoryBox
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="minha-nav" />
+                <Navbar.Collapse id="minha-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/home"> Produtos </Nav.Link>
+                        <Nav.Link href="/estoque"> Estoque </Nav.Link>
+                    </Nav>
+                    <Nav className="justify-content-end">
+                        <Navbar.Text style={{color:"white", marginRight:"5px"}}>
+                            {/* Caso usuario tenha feito login, aparece sair, se não aparece entrar */}
+                        {
+                            usuarioNome === "Visitante" ? (
+                                <>
+                                    <Button variant="primary" href="/login"
+                                    style={{width:"100px"}}
+                                    >
+                                        Entrar
+                                    </Button>
+                                </>
+                            ) : (
+                                <>
+                                    Usuario: {usuarioNome}
+                                    <Button variant="danger" href="/login"
+                                    style={{width:"100px", margin:"10px"}}
+                          onClick={logout}>
+                        Sair
+                      </Button>
+                    </>
+                  )
+                }
+              </Navbar.Text>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   );
 };
 
