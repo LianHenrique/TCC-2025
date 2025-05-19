ac3223447fb486c8f3b33445c468baead5630e55
Lian <lianhenrique1902@gmail.com>
2025-05-18 22:39:05 -0300

    corrigindo erro

--- src/pages/InitPages.jsx
+++ src/pages/InitPages.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router"
 import { AuthProvider } from '../Contexts/UserContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { AuthProvider } from "../Contexts/UserContext";
 
 const InitPages = () => {
   return (
