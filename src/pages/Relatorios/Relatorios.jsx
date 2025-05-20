6a989d26909e8f999d74b73684e44c4b19be697b
Wesley Pedreira Passos Rocha <wesley.p.rocha6@aluno.senai.br>
2025-05-20 16:30:44 -0300

    mais modificações

--- src/pages/Relatorios/Relatorios.jsx
+++ src/pages/Relatorios/Relatorios.jsx
@@ -66,11 +66,22 @@ const Relatorios = () => {
           insumo 6-tantasSaídas <br />
         </p>
 
-        
+
+        <Card className={styles.Card_Insumos}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
         
       </section>
-    </div> 
-  )  
+    </div>  
+  )   
 } 
 
 export default Relatorios
