53239a1461cb82fea6bdc1de18190ca717ee36e1
Wesley Pedreira Passos Rocha <wesley.p.rocha6@aluno.senai.br>
2025-06-03 16:16:15 -0300

    tem um bug que ao trocar de página, o componente é exibido novamente. porém ele só pode rodar se tiver um novo iten com pouco estoque. concertar esse erro

--- src/Notificacao/Notficacao.jsx
+++ src/Notificacao/Notficacao.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
+import { Container } from 'react-bootstrap';
+import Alert from 'react-bootstrap/Alert';
 
 const Notficacao = () => {
     const [resultados, setresultados] = useState([]);
+    const [visivel, setVisivel] = useState(false);
 
     useEffect(() => {
         fetch('http://localhost:3000/insumos/alerta')
@@ -22,23 +25,55 @@ const Notficacao = () => {
             })
             .catch(error => console.log(error));
 
+            setTimeout(() => {
+                setresultados([]);
+            }, 4000);
     }, [])
 
     return (
-        <div>
-            {resultados.length <= 10 && (
+        // <div> 
+        //         <section>
+        //             {resultados.length <= 10 && (
+        //                 <>
+        //                     <h1 class='h6'>ATENÇÃO, INSUMOS COM POUCA QUANTIDADE:</h1>
+        //                     <ul>
+        //                         {resultados.map((produto) => (
+        //                             <li key={produto.id_insumos}>
+        //                                 <p>{produto.nome_insumos} {produto.quantidade_insumos}</p>
+        //                             </li>
+        //                         ))}
+        //                     </ul>
+        //                 </>
+        //             )}
+        //         </section> 
+        // </div>
+
+        <Container style={{
+            position: 'fixed',
+            zIndex: 9999,
+            width: '20vw',
+            left: '80.5vw',
+            top: '2em',
+        }}>
+            {resultados.length > 0 && (
                 <>
-                    <h1>ATENÇÃO, INSUMOS COM POUCO ESTOQUE:</h1>
-                    <ul>
-                        {resultados.map((produto) => (
-                            <li key={produto.id_insumos}>
-                                <p>{produto.nome_insumos} {produto.quantidade_insumos}</p>
-                            </li>
-                        ))}
-                    </ul>
+                    <div>
+                        <Alert variant="danger">
+                            <Alert.Heading className='h6'>
+                                Atenção: Estoque reduzindo!
+                            </Alert.Heading>
+                            <ul style={{ paddingLeft: '1rem' }}>
+                                {resultados.map(produto => (
+                                    <li key={produto.id_insumos}>
+                                        {produto.nome_insumos} ({produto.quantidade_insumos})
+                                    </li>
+                                ))}
+                            </ul>
+                        </Alert>
+                    </div>
                 </>
             )}
-        </div>
+        </Container>
     )
 }
 
